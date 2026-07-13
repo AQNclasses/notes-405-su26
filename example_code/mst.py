82acc767ad9dbe53ec370dcc5f02ac51b1f3842d
Alli Nilles <anilles137@gmail.com>
2026-07-13 10:45:31 -0700

    update MST

--- example_code/mst.py
+++ example_code/mst.py
@@ -1,7 +1,7 @@
 from collections import deque
 import networkx as nx
 import matplotlib.pyplot as plt
-
+import random
 
 
 disconnected_graph = nx.watts_strogatz_graph(20,3,0.5,4)
@@ -13,8 +13,10 @@ connected_graph = nx.watts_strogatz_graph(20,3,0.5,5)
 # Output: NetworkX graph with same node and edge sets, randomized weights
 # Operates in place as well as returning graph
 def random_unique_weights(graph):
-    E = graph.number_of_edges
-    weights = 
+    E = graph.number_of_edges()
+    weights = random.sample(range(1,2*E), E)
+    for (u, v), w in zip(graph.edges, weights):
+        graph[u][v]['weight'] = w
 
 # Implement one of the MST algorithms we learned
 # Input: NetworkX graph
@@ -24,6 +26,11 @@ def mst(graph):
 
     return
 
+# Run algo and visualize
+
+graph = connected_graph # change for disconnected
+graph = random_unique_weights(graph)
+mst = mst(graph)
 
 # Visualization
 G = nx.Graph(graph)
@@ -33,5 +40,4 @@ plt.figure(figsize=(8, 6))
 nx.draw_networkx_nodes(G, pos, node_color='lightblue', node_size=700)
 nx.draw_networkx_edges(G, pos, edge_color='gray')
 nx.draw_networkx_labels(G, pos, font_size=12, font_weight='bold')
-plt.title("Graph for BFS Visualization")
 plt.show()
